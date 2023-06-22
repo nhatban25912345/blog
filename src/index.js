3f1be56d08521804d1d621288d5297a303644486
mnhthng2112 <dmnhthng@gmail.com>
2023-06-22 18:02:39 +0700

    mvc 1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { create } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 const hbs = create({extname: '.hbs'})
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,24 +24,9 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+//Routes init
+route(app);
 
-app.get('/news', (req, res) => {
-    res.render('news');
-});
 
-app.get('/search', (req, res) => {
-    console.log(req.query.q);
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-
-    console.log(req.body);
-
-    res.send("");
-});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+
+function route(app) {
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+    
+    // app.get('/news', (req, res) => {
+    //     res.render('news');
+    // });
+    
+    app.get('/search', (req, res) => {
+        console.log(req.query.q);
+        res.render('search');
+    });
+    
+    app.post('/search', (req, res) => {
+    
+        console.log(req.body);
+    
+        res.send("");
+    });
+}
+
+module.exports = route;
