b67fa862e84272f0c5ffc69c6d744f89a168ac0d
mnhthng2112 <dmnhthng@gmail.com>
2023-06-22 14:53:27 +0700

    POST and GET method

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ const hbs = create({extname: '.hbs'})
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', hbs.engine);
@@ -19,10 +24,22 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+    console.log(req.query.q);
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+
+    console.log(req.body);
+
+    res.send("");
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
