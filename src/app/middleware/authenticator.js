5466ad1dc25d964ea538070c11b7b9bb15812a36
mnhthng2112 <dmnhthng@gmail.com>
2023-07-28 12:48:14 +0700

    push data user sample

--- src/app/middleware/authenticator.js
+++ src/app/middleware/authenticator.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 verifyToken = (req, res, next) => {
   // test
-  const token = "eyJhbGciOiJIUzI1NiJ9.NjRjMzJhZGI2Y2ZiOTg3NjlhNzhkMzZi.fwUMCLIiueRJppbmcGdXUKjXpNh5xrLAgP5eyju7J5Q";
+  // const token = "eyJhbGciOiJIUzI1NiJ9.NjRjMzJhZGI2Y2ZiOTg3NjlhNzhkMzZi.fwUMCLIiueRJppbmcGdXUKjXpNh5xrLAgP5eyju7J5Q";
   // const token = { token:"" };
 
   // console.log(req);
-  // let token = req.headers.token;
+  let token = req.headers.token;
   // console.log("eyJhbGciOiJIUzI1NiJ9.NjRjMDdmMzBkMTllYTkyODhkZDViODY1.nsj-LQDVeQ4oRRZa7JyhRkeBuegcRtiRPd26y94fBOY");
 
   if (!token) {
