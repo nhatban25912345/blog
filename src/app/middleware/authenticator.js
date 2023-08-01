dee2e70197c73acddca85f03d498e00d33602152
nhatban25912345 <hoagnhat529@gmail.com>
2023-08-01 18:08:17 +0700

    merge branch front-end-dev to login

--- src/app/middleware/authenticator.js
+++ src/app/middleware/authenticator.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 verifyToken = (req, res, next) => {
   // test
-  const token = "eyJhbGciOiJIUzI1NiJ9.NjRjMzJhZGI2Y2ZiOTg3NjlhNzhkMzZi.fwUMCLIiueRJppbmcGdXUKjXpNh5xrLAgP5eyju7J5Q";
+  // const token = "eyJhbGciOiJIUzI1NiJ9.NjRjMzJhZGI2Y2ZiOTg3NjlhNzhkMzZi.fwUMCLIiueRJppbmcGdXUKjXpNh5xrLAgP5eyju7J5Q";
   // const token = { token:"" };
 
   // console.log(req);
-  // const token = req.headers.token;
+  const token = req.headers.token;
   
   if (!token) {
     return res.status(500).send({ message: "No token provided!" });
