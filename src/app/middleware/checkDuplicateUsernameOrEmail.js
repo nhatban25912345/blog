7f17acaac4a8f38db454c0d639799a25d72e2012
nhatban25912345 <hoagnhat529@gmail.com>
2023-08-02 11:12:28 +0700

    remove validate numberphone

--- src/app/middleware/checkDuplicateUsernameOrEmail.js
+++ src/app/middleware/checkDuplicateUsernameOrEmail.js
@@ -14,11 +14,11 @@ checkDuplicateUsernameOrEmail = async (req, res, next) => {
         return res.status(404).json({ code: 4, error: "Failed! Email was registered!" });
       }
 
-      // Kiểm tra sđt trùng lặp
-      const existingPhoneNumber = await User.findOne({ phonenumber: req.body.phonenumber }).exec();
-      if (existingPhoneNumber) {
-        return res.status(404).json({ code: 5, error: "Failed! Phonenumber was registered!" });
-      }
+      // // Kiểm tra sđt trùng lặp
+      // const existingPhoneNumber = await User.findOne({ phonenumber: req.body.phonenumber }).exec();
+      // if (existingPhoneNumber) {
+      //   return res.status(404).json({ code: 5, error: "Failed! Phonenumber was registered!" });
+      // }
 
       // Nếu cả tên người dùng email và số điện thoại đều là duy nhất, tiếp tục middleware tiếp theo
       next();
